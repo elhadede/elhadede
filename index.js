1c069d30131afe0c4aef4ddb57098cff6baa760d
a <a@core>
2021-08-11 11:45:54 +0300

    wow.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
+  var light="#3E2C41";
+  var dark="white";
+  $("#darkmode").on('click',function(){
+    dark = [light, light = dark][0];
+    $("body").css("--color1",dark);
+  });
   $('form').on('submit', function(e){
     e.preventDefault();
     var values = {};
     $.each($(this).serializeArray(), function(i, field) {
         values[field.name] = field.value;
     });
-    console.log(values);
   });
+});
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 300) {
+    $('#up').css("opacity","1");
+  } else {
+    $('#up').css("opacity","0");
+  }
 });
